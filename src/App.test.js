41c1be9d8f0225beeab4fd668b70103188852b9b
Cormac Doyle <doylec54@tcd.ie>
2022-01-12 13:33:30 +0000

    hello world with unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check for hello world text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/HelloWorldExample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HelloWorldExample() {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
