9923b1727d8d6e9c933bd5c7588625a7c82a60c5
claoidekemp <claoidek@tcd.ie>
2022-01-31 10:27:47 +0000

    Attempt at map display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MapContainer, TileLayer, } from "react-leaflet";
+import { render } from 'react-dom'
 import HelloWorldExample from './HelloWorldExample'
 
 function App() {
@@ -7,4 +9,18 @@ function App() {
   )
 }
 
+function Map() {
+  const position = [51.505, -0.09]
+
+  render(
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  )
+
+}
+Map();
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Map from './App/';
 
 test('check for hello world text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('check if map', () => {
+  const { container } = render(<Map />)
+  expect(container.childElementCount).toEqual(1);
 });
