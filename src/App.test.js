08ca5154f02e9defc64500c00667e4468e41fb7f
conoroheocha <conoroheocha@gmail.com>
2022-01-31 10:55:01 +0000

    added tests for links to other pages of site

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('check for hello world text', () => {
+const domain = "localhost:8000/";
+
+test("renders without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('has title', () => {
+  render(<App />);
+  expect(screen.getByRole('heading')).toHaveTextContent('Disaster Assessment');
+});
+
+test('have map link', () => {
+  render(<App />);
+  expect(screen.getByText('Disaster Map').closest('a')).toHaveAttribute('href', domain + "map");
+});
+
+test('have reporting link', () => {
+  render(<App />);
+  expect(screen.getByText('Report a Disaster').closest('a')).toHaveAttribute('href', domain + "report");
+});
+
+test('have login link', () => {
+  render(<App />);
+  expect(screen.getByText('Login').closest('a')).toHaveAttribute('href', domain + "login");
+});
+
+// test("Test theme button toggle", () => {
+//   render(<App />);
+//   const buttonEl = screen.getByText(/Current theme/i);
+    
+//   userEvent.click(buttonEl);
+//   expect(buttonEl).toHaveTextContent(/dark/i);
+// });
+
+
+
