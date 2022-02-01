a38e166b19fe9442fa33d889a860b634d631433d
tom337 <tom@rgrid.tech>
2022-02-01 11:47:56 +0000

    Location marker and custom map marker

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import L from 'leaflet';
 import "./Map.css";
 
@@ -14,14 +14,57 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
+const icon = L.icon({
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+    iconUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-shadow.png"
+});
+
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null)
+    const map = useMapEvents({
+        click() {
+            map.locate()
+        },
+        locationfound(e) {
+            setPosition(e.latlng)
+            map.flyTo(e.latlng, map.getZoom())
+        },
+    })
+
+    return position === null ? null : (
+        <Marker position={position}>
+            <Popup>There you are...</Popup>
+        </Marker>
+    )
+}
+
+function AddMarker() {
+    const [position, setPosition] = useState(null)
+    useMapEvents({
+        click: (e) => {
+            setPosition(e.latlng);
+        }
+    })
+    return position === null ? null : (
+        <Marker position={position}>
+            <Popup>Added position</Popup>
+        </Marker>
+    )
+}
+
 const Map = () => {
 
     const defaultPosition = [53.348, -6.2603];  // Dublin City Centre
+
     return (<div className="map__container">
         <MapContainer
             center={defaultPosition}
             zoom={13}
-            scrollWheelZoom={false}
+            scrollWheelZoom={true}
             style={{ height: "100vh" }}
             zoomControl={false}
         >
@@ -29,13 +72,9 @@ const Map = () => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={defaultPosition}>
-                <Popup>
-                    An example marker. This pop-up will feature traffic details.
-                </Popup>
-            </Marker>
+            <LocationMarker />
+            <AddMarker />
         </MapContainer>
     </div>);
 };
-
-export default Map;
+export default (Map);
