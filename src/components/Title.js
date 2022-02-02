5137b5452362d0682f6a5c17267fa0d2cdd58fbf
Cormac Doyle <doylec54@tcd.ie>
2022-02-02 17:49:59 +0000

    actions

--- src/components/Title.js
+++ src/components/Title.js
@@ -11,7 +11,7 @@ export default function Title() {
                 alignItems: "center",
                 backgroundColor: "white"
             }}
-            
+     
         >
             Disaster Assesment Application
         </div>
