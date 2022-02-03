97eb14883a94d914a1881eae097f5d9b1ccbf0dc
claoidekemp <claoidek@tcd.ie>
2022-02-03 16:35:32 +0000

    Dummy button added

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,19 +2,36 @@ import React from 'react'
 
 export default function Title() {
     return (
-        <div
-            style={{
-                fontSize: 30,
-                fontWeight: 200,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "white"
-            }}
-     
-        >
-            Disaster Assesment Application
+        <div>
+
+            <button
+                style={{ backgroundColor: 'red' }}
+                className='btn'>
+                {"Click Me"}
+            </button>
+
+            <div
+                style={{
+                    fontSize: 30,
+                    fontWeight: 200,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "white"
+                }}
+
+            >
+                Disaster Assesment Application
+            </div>
         </div>
-        
+
     )
 }
+/*
+<button 
+onClick={onClick}
+style={{backgroundColor:'red' }} 
+className='btn'>
+{"Click Me"}
+</button>
+*/
