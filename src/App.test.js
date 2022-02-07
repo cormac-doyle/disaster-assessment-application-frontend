54eadcb5b667e9aaa01a7ecdb263b8f0d0532b61
Cormac Doyle <doylec54@tcd.ie>
2022-02-07 10:44:59 +0000

    mock test currently failing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Map from "./components/Map/Map";
+import {fetchResponseJson} from './fetchResponseJson'
 
 test('check for title text', () => {
   render(<App />);
@@ -11,4 +12,19 @@ test('check for title text', () => {
 test('check if map', () => {
   const { container } = render(<Map />)
   expect(container.childElementCount).toEqual(1);
+});
+
+const MOCK_MESSAGE = {"message":"Hello World"}
+
+test('check data has been loaded', async () => {
+  fetch = jest.fn(() =>
+  Promise.resolve({
+      json: () => Promise.resolve(MOCK_MESSAGE),
+    })
+  );
+
+  const data = await fetchResponseJson('test-url')
+  expect(fetch).toBeCalledWith("test-url");
+  expect(data).toEqual(MOCK_MESSAGE)
+
 });
