3058c063c6892eaa3fbeab19862bdbc4227d995d
conoroheocha <conoroheocha@gmail.com>
2022-02-14 10:29:49 +0000

    test folder

---
+++ src/components/home/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "../../App.css";
+import { Component } from 'react/cjs/react.production.min';
+import Map from "../map/MapTraffic"
+import Title from "../Title"
+import DisasterStatus from "../DisasterStatus";
+import { fetchResponseJson } from './fetchResponseJson'
+import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import Alert from 'react-bootstrap/Alert';
+
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+        }
+    }
+
+    componentDidMount() {
+        return fetchResponseJson('http://localhost:8000/').then((responseJson) => {
+            this.setState({
+                items: responseJson
+            })
+        })
+    }
+
+    render() {
+        return (<>
+            <nav>
+            </nav>
+            <main>
+                <Title />
+                <GDPRAlert />
+                <DisasterStatus items={this.state.items}></DisasterStatus>
+                <Map />
+
+            </main>
+        </>)
+
+    }
+};
+
+export default Home;
+
+
+function GDPRAlert() {
+    const [show, setShow] = useState(true);
+
+    if (show) {
+        return (
+            <Alert variant="primary" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>Disclaimer</Alert.Heading>
+                <p>
+                    By proceeding with this application you are consenting to the use of your location data for routing purposes
+                </p>
+            </Alert>
+        );
+    }
+    return null;
+}
---
+++ src/tests/Home.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../Home';
+import MapTraffic from "../components/map/MapTraffic";
+import { fetchResponseJson } from '../components/home/fetchResponseJson'
+
+test('check for title text', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Disaster Assesment Application/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('check if map exists', () => {
+  const { container } = render(<MapTraffic />)
+  expect(container.childElementCount).toEqual(1);
+});
+
+const MOCK_MESSAGE = { "message": "Hello World" }
+
+test('check mock data has been loaded', async () => {
+  fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(MOCK_MESSAGE),
+    })
+  );
+
+  const data = await fetchResponseJson('test-url')
+  expect(fetch).toBeCalledWith("test-url");
+  expect(data).toEqual(MOCK_MESSAGE)
+
+});
