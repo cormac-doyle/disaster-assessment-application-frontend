7a75f885b49650131daed34c35c99cbcf33d9f58
claoidekemp <claoidek@tcd.ie>
2022-02-16 16:49:08 +0000

    Update Report.test.js

--- src/tests/Report.test.js
+++ src/tests/Report.test.js
@@ -10,25 +10,52 @@ test('check if map exists', () => {
     expect(container.childElementCount).toEqual(1);
 });
 
-//TODO popup test
-/*
+let MOCK_RESPONSE = JSON.stringify({
+    user_id: 0,
+    disaster_id: 99,
+    scale: 18,
+    disaster_type: "fire",
+    long: 123.456,
+    lat: 123.456,
+});
+
+const unmockedFetch = global.fetch
+
+beforeAll(() => {
+    global.fetch = () =>
+        Promise.resolve({
+            json: () => Promise.resolve(MOCK_RESPONSE),
+        })
+})
+
+afterAll(() => {
+    global.fetch = unmockedFetch
+})
+
 test('modal shows the children and a close button', () => {
     // Arrange
     const handleClose = jest.fn()
 
     // Act
-    const { getByText } = render(<ReportDisasterPopUp />)
+    let position = {
+        lng: 123.456,
+        lat: 123.456
+    }
 
-    getByText.props.position.lng = 123.456;
+    render(<ReportDisasterPopUp position={position} />);
 
-    getByText.props.position.lat = 123.456;
     // Assert
-    expect(getByText('Please select disaster scale and type')).toBeTruthy()
+    expect(screen.getByText('Disaster Report')).toBeTruthy()
+    //const linkElement = screen.getByText(/Disaster Report/i);
+    //expect(linkElement).toBeInTheDocument();
 
     // Act
     fireEvent.click(getByText(/close/i))
 
     // Assert
     expect(handleClose).toHaveBeenCalledTimes(1)
-})
-*/
+});
+
+//TODO popup test
+
+
