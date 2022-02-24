ba5258f675ce757c59a3435abafc7fb2d1a4787f
claoidekemp <claoidek@tcd.ie>
2022-02-24 16:35:27 +0000

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -14,6 +14,43 @@ class Login extends Component {
         super(props);
         this.state = {
             items: [],
+            email: "",
+            password: "",
+        };
+    }
+
+    onSubmit = () => {
+        console.log(this.state.email);
+        console.log(this.state.password);
+        this.postEmailAndPassword()
+    };
+
+    async postEmailAndPassword() {
+
+        const requestOptions = {
+            method: "post",
+            //mode: 'no-cors',
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                username: this.state.email,
+                password: this.state.password,
+            }),
+        };
+
+        try {
+
+            let emailAndPasswordJSON = await fetch("http://localhost:8000/api/1/token", requestOptions).then(response => response.json());
+
+            alert("Email and Password Sent: " + JSON.stringify(emailAndPasswordJSON));
+            this.props.onHide();
+
+        } catch (e) {
+            alert("Login Failed");
+
+            this.props.onHide();
+            console.log(e);
         }
     }
 
@@ -29,14 +66,14 @@ class Login extends Component {
                         <Form>
                             <Form.Group className="mx-5" controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" onChange={e => this.setState({ value: e.target.value })} value={this.state.value} />
+                                <Form.Control type="email" placeholder="Enter email" onChange={e => this.setState({ email: e.target.value })} value={this.state.email} />
                             </Form.Group>
 
                             <Form.Group className="mx-5" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" placeholder="Password" onChange={e => this.setState({ password: e.target.value })} value={this.state.password} />
                             </Form.Group>
-                            <Button variant="primary" className="m-5" type="submit" disabled={!this.state.value}>
+                            <Button variant="primary" className="m-5" type="submit" disabled={!this.state.email} onClick={this.onSubmit}>
                                 Submit
                             </Button>
                         </Form>
