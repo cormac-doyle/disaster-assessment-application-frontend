e957dbe4d1a83291599a7f9ddf3ae3e79258bfda
Cormac Doyle <doylec54@tcd.ie>
2022-02-28 13:26:42 +0000

    not working

--- src/components/map/RoutingMachine.js
+++ src/components/map/RoutingMachine.js
@@ -1,14 +1,38 @@
-import L from "leaflet";
+
 import { createControlComponent } from "@react-leaflet/core";
-import "leaflet-routing-machine";
+var L = require('leaflet');
+require('leaflet-routing-machine');
+require("lrm-tomtom")
 
 const createRoutingMachineLayer = (props) => {
+
     const instance = L.Routing.control({
+        router: new L.Routing.TomTom('QqBwGI5EkL9fqOrncf0jZI1VUiYOtXbW',
+        {
+            serviceUrl: "https://api.tomtom.com/routing/1/calculateRoute",
+            timeout: 30 * 1000,
+            routeType: "fastest", // fastest, shortest, eco, thrilling
+            language: "", // en-GB
+            instructionsType: "", // coded, text, tagged
+            traffic: true,
+            avoid: "", // [tollRoads, motorways, ferries, unpavedRoads, carpools, alreadyUsedRoads]
+            travelMode: "car", // car, truck, taxi, bus, van, motorcycle, bicycle, pedestrian
+            vehicleMaxSpeed: 0, // km/h
+            vehicleWeight: 0, // kg
+            vehicleAxleWeight: 0, // kg
+            vehicleLength: 0, // m
+            vehicleWidth: 0, // m
+            vehicleHeight: 0, // m
+            departAt: "", // YYYY-MM-DD\THH:mm:ss
+            arriveAt: "", // YYYY-MM-DD\THH:mm:ss
+            vehicleCommercial: false
+        }),
         waypoints: props.waypoints,
         createMarker: function() { return null; },
         lineOptions: {
             styles: [{ color: "#6FA1EC", weight: 4 }]
         },
+        
         show: false,
         addWaypoints: false,
         routeWhileDragging: true,
