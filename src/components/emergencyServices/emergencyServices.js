e524a65f9a417859b95a986270dc26dc4633c31b
Tom Mulligan <mulligto@tcd.ie>
2022-03-11 12:23:47 +0000

    Disaster Verification interface

--- src/components/emergencyServices/emergencyServices.js
+++ src/components/emergencyServices/emergencyServices.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Component } from 'react/cjs/react.production.min';
-import Map from "../map/MapDisaster"
+import Map from "../map/MapVerify"
 import Title from "../Title"
 import UserStatus from "../UserStatus";
 // import { fetchResponseJson } from './fetchResponseJson'
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { fetchResponseJson } from '../fetchResponseJson'
 
 
 
