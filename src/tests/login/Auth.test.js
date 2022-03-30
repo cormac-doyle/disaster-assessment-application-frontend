1b8e69cf076ea09b931dfbd36fe5910108e4a64f
daltHub <57568091+daltHub@users.noreply.github.com>
2022-03-30 16:52:26 +0100

    removed stuff from Auth.test.js

--- src/tests/login/Auth.test.js
+++ src/tests/login/Auth.test.js
@@ -1,6 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
 import {setToken, fetchToken, RequireToken} from '../../components/login/Auth';
-import { BrowserRouter } from "react-router-dom";
+
 
 test('token set and fetched', () => {
     setToken('something')
