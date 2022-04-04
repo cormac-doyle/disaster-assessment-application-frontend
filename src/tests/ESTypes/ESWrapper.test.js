027b015abcef78e1f40e446c6692abdfec37d9c6
conoroheocha <conoroheocha@gmail.com>
2022-04-04 11:01:37 +0100

    minor changes

---
+++ src/tests/ESTypes/ESWrapper.test.js
@@ -0,0 +1,43 @@
+import getES from "../../components/ESTypes/ESWrapper";
+import L from "leaflet";
+
+const ESIconParams = [
+    //[param, result]
+    [2, L.icon({
+        iconUrl: "hospital.png",
+        iconSize: [40, 40],
+        iconAnchor: [10, 41],
+        popupAnchor: [2, -40],
+    })],
+
+    [1, L.icon({
+        iconUrl: "firestation.png",
+        iconSize: [40, 40],
+        iconAnchor: [10, 41],
+        popupAnchor: [2, -40],
+    })],
+
+    [0, L.icon({
+        iconUrl: "policeIcon.png",
+        iconSize: [40, 40],
+        iconAnchor: [10, 41],
+        popupAnchor: [2, -40],
+    })]
+];
+
+
+
+
+//run parameterised test on enumerations
+//const disasterLocations = new DisasterLocations()
+
+describe("test getES Icons", () => {
+    test.each(ESIconParams)(
+        `should return correct icon`,
+        (input, result) => {
+            const output = getES(input).icon
+
+            expect(output).toEqual(result);
+        }
+    );
+});
